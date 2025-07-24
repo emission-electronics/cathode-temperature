371b1f63ba0ed895421558ddda251d3cb2f93bf1
Gleb Lugovtsov <galera951@gmail.com>
2025-07-24 13:41:12 +0300

    Edit roicam

--- ROICAM.bat
+++ ROICAM.bat
@@ -1,8 +1,8 @@
 @ECHO off
 
 call .venv\Scripts\activate.bat
-python roi_camera_capture\main.py
+python src\roi_camera_capture\main.py
 
-ECHO Application closed successfully.
+ECHO Application closed.
 
 pause
