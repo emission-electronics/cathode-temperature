4c61c8cc243f2a90713170676d90c4afeeadee85
Gleb Lugovtsov <galera951@gmail.com>
2025-07-24 14:50:36 +0300

    Add graduation processing script

---
+++ src/method_processing/cmd_grad.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from logging import getLogger as lg
+import os
+import re
+
+import numpy as np
+import click
+
+from util import export_grad_poly, import_image
+
+# TODO: T on I calibration
+T = lambda I: 108.0958765*I*I*I-511.9765339*I*I+1617.95649045*I+537.60415503
+
+# IMPORTANT: for vertical wire
+def wire_brightness(image: np.ndarray, choose_percentage: float = .3) -> tuple[np.floating, np.floating]:
+    def j_maxs(img_array):
+        res = []
+        for i in range(img_array.shape[0]):
+            res.append(np.argmax(img_array[i]))
+        return np.array(res)
+    
+    i_arr = np.arange(image.shape[0])
+    j_max = j_maxs(image)
+    Y_max = image[i_arr, j_max]
+
+    # filter very low values
+    idx = (Y_max > Y_max.mean() * .8)
+    i_arr = i_arr[idx]
+    j_max = j_max[idx]
+    Y_max = image[i_arr, j_max]
+
+    # choose_percentage points in center
+    center = len(Y_max) // 2
+    shift = int(len(Y_max) * choose_percentage) // 2
+    Y_max = Y_max[center - shift:center + shift + 1]
+
+    return np.mean(Y_max), np.std(Y_max)
+
+def grad_poly_fit(means: np.ndarray, temperatures: np.ndarray, fit_order: int = 5) -> np.poly1d:
+    popt, _ = np.polyfit(means, temperatures, fit_order, cov=True, full=False)
+    return np.poly1d(popt)
+
+@click.command()
+@click.option("--overwrite", is_flag=True, help="Разрешить перезапись градуировки")
+@click.pass_context
+def grad(ctx: click.Context, overwrite: bool):
+    cwd = Path().cwd()
+    grad_dir = cwd / str(ctx.obj['GRAD_DIR'])
+    grad_name = ctx.obj['GRAD_NAME']
+    grad_path = grad_dir / f"{grad_name}.grad"
+
+    click.echo(f"Using graduation {grad_name}")
+
+    if grad_path.exists():
+        if not overwrite:
+            click.echo(f"Graduation file {grad_path} already exist")
+            return
+        grad_path.unlink()
+
+    means = []
+    temperatures = []
+    try:
+        for filename in os.listdir(grad_dir):
+            if not filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                continue
+            
+            image = import_image(grad_dir / filename)
+
+            match = re.search(r'\d{4}', filename)
+            if match is None:
+                lg(__name__).error(f"There is no current value in filename: {filename}")
+                continue
+
+            current = np.float64(match.group()) / 1000
+            temperature = T(current)
+            mean, _ = wire_brightness(image)
+
+            click.echo(f"Processed {filename}:\t{current=:.2f}A\t{temperature=:.2f}K\t{mean=:.2f}")
+            means.append(mean)
+            temperatures.append(temperature)
+        
+        p = grad_poly_fit(np.array(means), np.array(temperatures))
+        click.echo("Result:")
+        click.echo(p)
+        click.echo()
+
+        export_grad_poly(grad_path, p)
+        click.echo(f"Graduation file {grad_path} created")
+            
+    except FileNotFoundError as e:
+        lg(__name__).error(f"File not found: {e}")
+    except Exception as e:
+        lg(__name__).error(f"Error processing files: {e}")
--- src/method_processing/main.py
+++ src/method_processing/main.py
@@ -1,7 +1,9 @@
 import click
 import logging
 import sys
+
 from cmd_apply import apply
+from cmd_grad import grad
 
 # Configure logging to stdout
 logging.basicConfig(
@@ -11,15 +13,20 @@ logging.basicConfig(
 )
 
 @click.group()
-@click.option("--grad-name", default="ns11_ns77", help="Название градуировки (без расширения .grad)")
+@click.option("--debug", is_flag=True, help="Включить логгинг дебага")
+@click.option("--grad-name", default="ns11_ns7", help="Название градуировки (без расширения .grad)")
 @click.option("--grad-dir", default="GRADUATION", help="Каталог с файлами градуировок")
 @click.pass_context
-def main(ctx, grad_name, grad_dir):
+def main(ctx: click.Context, debug: bool, grad_name: str, grad_dir: str):
+    if debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+        
     ctx.ensure_object(dict)
     ctx.obj['GRAD_NAME'] = grad_name
     ctx.obj['GRAD_DIR'] = grad_dir
 
 main.add_command(apply)
+main.add_command(grad)
 
 if __name__ == "__main__":
     main()
